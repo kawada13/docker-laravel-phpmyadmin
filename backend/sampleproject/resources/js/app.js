d36713e8423a502c099ec110e178c70b15830552
kawada <vgnsl94@gmail.com>
2021-04-30 17:50:09 +0900

    s

--- backend/sampleproject/resources/js/app.js
+++ backend/sampleproject/resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue';
+import Vuetify from './plugins/vuetify' // vuetify.jsを読み込み
+import routes from './router/index'
+
+
+import ExampleComponent from './components/ExampleComponent.vue'
+Vue.component('app-header', require('./components/Header.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    vuetify: Vuetify,
+    router:routes,
+});
