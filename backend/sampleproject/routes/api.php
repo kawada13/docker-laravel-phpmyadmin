274aacd2d3965cb2cc434ff40030891df1908a03
kawada <vgnsl94@gmail.com>
2021-05-03 14:51:42 +0900

    s

--- backend/sampleproject/routes/api.php
+++ backend/sampleproject/routes/api.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', 'AuthController@login');
-Route::post('/register', 'AuthController@register');
-Route::post('/logout', 'AuthController@logout');
-Route::resource('category', 'CategoryController');
+Route::post('/login', 'AuthController@login')->name('login');
+Route::post('/register', 'AuthController@register')->name('register');
+Route::post('/logout', 'AuthController@logout')->name('logout');
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -28,5 +28,5 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
+    Route::resource('category', 'CategoryController');
 });
--- backend/sampleproject/tests/Feature/CategoryTest.php
+++ backend/sampleproject/tests/Feature/CategoryTest.php
@@ -7,10 +7,19 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Category;
 use App\Product;
+use App\User;
 
 class CategoryTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // テストユーザー作成
+        $this->user = factory(User::class)->create();
+    }
     /**
      * A basic feature test example.
      *
@@ -18,10 +27,11 @@ class CategoryTest extends TestCase
      */
     public function test_index()
     {
+
         factory(Category::class, 5)->create();
 
-        $response = $this->json('GET', route('category.index'));
-
+        $response = $this->actingAs($this->user)
+                         ->json('GET', route('category.index'));
         $response
             ->assertStatus(200)
             ->assertJson(['message' => '成功']);
